7e680118716b416e77f1fdce07a478a99eb0e664
PiotrSwitlicki <PiotrSwitlicki2@gmail.com>
2022-08-21 20:01:30 +0200

    crud update

--- routes/web.php
+++ routes/web.php
@@ -13,27 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
-
-//Route::get('/', [App\Http\Controllers\TodoController::class, 'index'])->name('main');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
-
-
-//Route::post('/message/create', [App\Http\Controllers\TodoController::class, 'store'])->name('todostore');
-//Route::delete('/message/{message}', [App\Http\Controllers\TodoController::class, 'destroy'])->name('destroy');
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/create', [App\Http\Controllers\HomeController::class, 'create'])->name('products.add');
 Route::post('/store', [App\Http\Controllers\HomeController::class, 'store'])->name('products.store');
